24c10e8879fa61af11ff6d3f6df71911e53f6975
sri harsha <harsha312006@gmail.com>
2025-10-29 19:32:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://smart-canteen-backend-c9ge.onrender.com',
         changeOrigin: true,
         secure: false,      
       },
